2ce400f66975f26fcfce17eca6654aec6f42afc7
Arifia Kasastra R <arka.progammer@gmail.com>
2022-10-03 16:40:55 +0700

    Changing AuthController using UserService

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,5 +1,7 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import User from 'App/Models/User'
+import UserService from 'App/Services/UserServices'
+
+let userService = new UserService()
 
 export default class AuthController {
     public async login({ request, auth }: HttpContextContract) {
@@ -15,29 +17,25 @@ export default class AuthController {
         }
     }
 
-    public async logout({ auth }: HttpContextContract) {
-        await auth.use('api').revoke()
-        return { 
-            status: 200,
-            message: 'Logout success'
-         }
-    }
-
     // register
     public async register({ request, auth }: HttpContextContract) {
-        const email = request.input('email')
-        const password = request.input('password')
-        const name = request.input('name')
-        const user = await User.create({ email, password, name })
-        
-        await user.save();
-        const token = await auth.use('api').attempt(email, password, {
-            expiresIn: '1 day',
+        const user = await userService.createUser({ ...request.all() })
+
+        const token = await auth.use('api').login(user, {
+            expiresIn: '10 days',
         })
         return {
             status: 200,
             message: 'Register success',
             data: token.toJSON(),
         }
+    }
+
+    public async logout({ auth }: HttpContextContract) {
+        await auth.use('api').revoke()
+        return { 
+            status: 200,
+            message: 'Logout success'
+         }
     }
 }
