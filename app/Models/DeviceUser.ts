88691cc44b6a0223301a2bc618863e2a71ee0373
Arifia Kasastra R <arka.progammer@gmail.com>
2022-10-05 14:24:07 +0700

    Creating Device User model and migration

--- app/Models/DeviceUser.ts
+++ app/Models/DeviceUser.ts
@@ -1,25 +1,29 @@
 import { DateTime } from 'luxon'
-import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, belongsTo, BelongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import Device from './Device'
 import User from './User'
 
-export default class Device extends BaseModel {
+export default class DeviceUser extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public deviceId: number
 
   @column()
   public userId: number
 
   @column()
-  public name: string
+  public isActive: boolean
 
-  @column()
-  public macAddress: string
-  
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @belongsTo(() => Device)
+  public device: BelongsTo<typeof Device>
 
   @belongsTo(() => User)
   public user: BelongsTo<typeof User>
