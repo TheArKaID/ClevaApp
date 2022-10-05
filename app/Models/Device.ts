9f19bee3be69edea0d0e8b582d5ebe9cc9e89d28
Arifia Kasastra R <arka.progammer@gmail.com>
2022-10-05 13:26:26 +0700

    Adding user_id to Devices

--- app/Models/Device.ts
+++ app/Models/Device.ts
@@ -1,9 +1,13 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
 
 export default class Device extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public user_id: number
 
   @column()
   public name: string
@@ -16,4 +20,7 @@ export default class Device extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
 }
--- database/migrations/1664790128030_devices.ts
+++ database/migrations/1664790128030_devices.ts
@@ -6,6 +6,7 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.integer('user_id').unsigned().references('users.id').onDelete('CASCADE')
       table.string('name', 255).notNullable()
       table.string('mac_address', 50).notNullable()
 
