94b4c4619cb1c4e7f55b0ac0a22d8a8a44b7b8a5
Arifia Kasastra R <arka.progammer@gmail.com>
2022-10-06 08:49:22 +0700

    Updating Device Services

--- app/Services/DeviceService.ts
+++ app/Services/DeviceService.ts
@@ -1,4 +1,6 @@
 import Device from "App/Models/Device"
+import DeviceUser from "App/Models/DeviceUser"
+import User from "App/Models/User"
 
 export default class DeviceService {
     // Get all devices
@@ -22,6 +24,9 @@ export default class DeviceService {
     // Create device
     public async createDevice(data: any) {
         const device = await Device.create(data)
+
+        this.grantDevice(device.id, data.user_id)
+        
         return device
     }
 
@@ -42,5 +47,71 @@ export default class DeviceService {
             await device.delete()
             return device
         }
+    }
+
+    // Grant user access to device
+    public async grantDevice(device_id: number, user_id: number, owner_id: number = 0) {
+        if (await this.isOwnedByUser(device_id, owner_id)) {
+            return 'User does not own device'
+        }
+        
+        let deviceUser = await this.checkDeviceAccess(device_id, user_id)
+        if (deviceUser) {
+            return deviceUser.merge({ isActive: true }).save()
+        }
+
+        const device = await Device.find(device_id)
+        const user = await User.find(user_id)
+        if (device && user) {
+            const deviceUser = await DeviceUser.create({
+                deviceId: device_id,
+                userId: user_id,
+            })
+            return deviceUser
+        }
+        return 'Device or user not found'
+    }
+
+    // Get all Devices owned by User
+    public async getDevicesByUser(user_id: number|undefined) {
+        const devices = await Device.query().where('user_id', user_id || 0)
+        return devices
+    }
+    
+    // Revoke user access to device
+    public async revokeDevice(device_id: number, user_id: number) {
+        if (await this.isOwnedByUser(device_id, user_id)) {
+            return 'User does not own device'
+        }
+        const deviceUser = await DeviceUser.query().where('device_id', device_id).where('user_id', user_id).first()
+        if (deviceUser) {
+            await deviceUser.merge({ isActive: false }).save()
+            return deviceUser
+        }
+        return null
+    }
+
+    // Get all devices that can accessed by a user
+    public async getUserDevices(user_id: number) {
+        const deviceUsers = await DeviceUser.query().where('user_id', user_id).preload('device')
+        return deviceUsers
+    }
+
+    // Check if user has access to device
+    public async checkDeviceAccess(device_id: number, user_id: number) {
+        const deviceUser = await DeviceUser.query().where('device_id', device_id).where('user_id', user_id).first()
+        if (deviceUser) {
+            return deviceUser
+        }
+        return false
+    }
+
+    // Check if user owns device
+    public async isOwnedByUser(device_id: number, owner_id: number) {
+        const device = await Device.query().where('id', device_id).where('user_id', owner_id).first()
+        if (device) {
+            return true
+        }
+        return false
     }
 }
