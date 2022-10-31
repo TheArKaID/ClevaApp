74803c7cf036c17a23f654f1897916d8b48b5cf1
Arifia Kasastra R <arka.progammer@gmail.com>
2022-10-31 13:00:30 +0700

    Adding Exception Handler for some code

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -14,10 +14,37 @@
 */
 
 import Logger from '@ioc:Adonis/Core/Logger'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   constructor() {
     super(Logger)
   }
+  public async handle(error: any, ctx: HttpContextContract) {
+    /**
+     * Self handle the validation exception
+     */
+    if (ctx.request.url().split('/')[1] == 'api') {
+      if (error.code === 'E_ROW_NOT_FOUND') {
+        console.log(Object.getPrototypeOf(Object.getPrototypeOf(error).constructor).name)
+        return ctx.response.send({
+          'status': 400,
+          'message': 'Failed. ' + typeof error + ' Not Found.'
+        })
+      }
+
+      if (error.code === 'E_ROUTE_NOT_FOUND') {
+        return ctx.response.send({
+          'status': 404,
+          'message': 'Failed. Resources not Found.'
+        })
+      }
+    }
+    /**
+     * Forward rest of the exceptions to the parent class
+     */
+    return super.handle(error, ctx)
+  }
+
 }
