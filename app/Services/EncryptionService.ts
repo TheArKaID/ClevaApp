e0919fda0d0ac473129518c4e9a4fe5a0faac3a0
Arifia Kasastra R <arka.progammer@gmail.com>
2023-02-07 11:49:25 +0700

    Updating Encryption Service

--- app/Services/EncryptionService.ts
+++ app/Services/EncryptionService.ts
@@ -1,25 +1,40 @@
 import crypto from 'crypto'
 
-const algorithm = 'aes-128-ecb';
-const iv = null;
+const algorithm = 'aes-128-ecb'
 
 export default class EncryptionService {
     public async encrypt(text: string, key: string) {
-        key = key.substring(0, 16);
-        var cipher = crypto.createCipheriv(algorithm, key, iv);
-
-        var mystr = cipher.update(text, 'utf8', 'base64')
-        mystr += cipher.final('base64');
-
-        return mystr;
+        key = key.substring(0, 16)
+        let thekey = await this.atohex(key)
+        const iv = null
+        var cipher = crypto.createCipheriv(algorithm, thekey, iv)
+        cipher.setAutoPadding(true)
+        var mystr = cipher.update(await this.atohex(text))
+        return Buffer.concat([mystr, cipher.final()]).toString('base64')
     }
 
-    public async decrypt(text: string, key: string) {
-        key = key.substring(0, 16);
-        var cipher = crypto.createDecipheriv(algorithm, key, iv);
-        var mystr = cipher.update(text, 'base64', 'utf8')
-        mystr += cipher.final('utf8');
+    public async decrypt(cipher: string, key: string) {
+        key = key.substring(0, 16)
+        let thekey = await this.atohex(key)
+        let thedeciper = await this.b64tohex(cipher)
+        const iv = null
+        var decipher = crypto.createDecipheriv(algorithm, thekey, iv)
+        decipher.setAutoPadding(false)
+        var mystr = decipher.update(thedeciper).toString('utf8')
 
+        // Remove padding
+        mystr = mystr.replace(/[\x00-\x1F\x80-\xFF]/g, '')
+        
         return mystr
+    }
+
+    // Base64 to hex
+    public async b64tohex(str: string) {
+        return Buffer.from(str, 'base64')
+    }
+
+    // ASCII to hex
+    public async atohex(str: string) {
+        return Buffer.from(str, 'ascii')
     }
 }
